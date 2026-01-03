aa77c1edb584f32df74eb97ad178183627d56bf4
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-03 13:37:12 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
+
+const rootEl = document.getElementById("root");
+
+if (!rootEl) {
+  throw new Error("Не найден #root в index.html");
+}
+
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
