b7643806d98460922df7932f8c2302feccd247f2
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-04 16:52:53 +0300

    Integrate demo data into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
 import { useState } from "react";
+
 import { DriverInfo } from "./components/DriverInfo";
 import { TripMap } from "./components/TripMap";
 import { TripDetails } from "./components/TripDetails";
 import { RelaxationModal } from "./components/RelaxationModal";
 
+import { demoDriver, demoTrip } from "./data/demoTrip";
+
 export default function App() {
+  // Состояние: открыта ли модалка релакса
   const [isRelaxOpen, setIsRelaxOpen] = useState(false);
 
   return (
     <div className="page">
+      {/* Верхняя панель */}
       <header className="header">
         <h1 className="title">Taxi demo</h1>
-        <button className="btn" onClick={() => setIsRelaxOpen(true)}>
+
+        <button
+          className="btn"
+          onClick={() => setIsRelaxOpen(true)}
+        >
           Open relax
         </button>
       </header>
 
+      {/* Основной контент */}
       <main className="grid">
-        <DriverInfo />
-        <TripMap />
-        <TripDetails />
+        <DriverInfo
+          name={demoDriver.name}
+          rating={demoDriver.rating}
+          car={demoDriver.car}
+        />
+
+        <TripMap
+          from={demoTrip.from}
+          to={demoTrip.to}
+        />
+
+        <TripDetails
+          etaMin={demoTrip.etaMin}
+          price={demoTrip.price}
+        />
       </main>
 
+      {/* Модалка */}
       <RelaxationModal
         open={isRelaxOpen}
         onClose={() => setIsRelaxOpen(false)}
