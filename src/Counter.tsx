c4a7ceff2b617b617eda2fae339af07affadb631
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-04 14:43:24 +0300

    Delete src/Counter.tsx

--- src/Counter.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  value: number;
-  onIncrement: () => void;
-};
-
-export default function Counter({ value, onIncrement }: Props) {
-  return <button onClick={onIncrement}>{value}</button>;
-}
