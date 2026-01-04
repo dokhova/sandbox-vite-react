c6f0fdd027074aac705e8175db52488c9666ec1a
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-04 15:48:50 +0300

    Create demoTrip.ts

---
+++ src/data/demoTrip.ts
@@ -0,0 +1,13 @@
+export const demoDriver = {
+  name: "Alex",
+  rating: 4.9,
+  car: "Toyota Prius",
+};
+
+export const demoTrip = {
+  from: "City Center",
+  to: "Airport",
+  etaMin: 12,
+  price: 8.4,
+};
+900
