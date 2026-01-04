11249aa6ab4a3f3fc66ff36d4bae89017e33fe5b
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-04 14:43:41 +0300

    Delete src/log.ts

--- src/log.ts
+++
@@ -1,8 +0,0 @@
-export function log(step: string, data?: unknown) {
-  const time = new Date().toLocaleTimeString();
-  if (data === undefined) {
-    console.log(`[${time}] ${step}`);
-  } else {
-    console.log(`[${time}] ${step}`, data);
-  }
-}
