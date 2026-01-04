170bce58f778caf40ca9066524343054e171a775
Alica Dokhova <dokhova.alica@gmail.com>
2026-01-04 12:36:14 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+import "./index.css";
 
-const rootEl = document.getElementById("root");
-
-if (!rootEl) {
-  throw new Error("Не найден #root в index.html");
-}
-
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
